07966b6bdf0cb13150bdd00ec56d5060b5f7b815
rupesh-raj-9142 <rajrupesh824143@gmail.com>
2025-12-15 15:46:35 +0530

    15.12 commit

--- About/index.js
+++ About/index.js
@@ -1,10 +1,11 @@
-console.log('hello')
-console.log('hi');
-console.log("byyy");
-console.log("new");
+console.log('Hello')
 
 console.log('Rupesh C')
 console.log('Nice')
 
 console.log("Hello byhy");
 console.log("last");
+
+
+console.log("Arpit");
+dkjjdfnclk
